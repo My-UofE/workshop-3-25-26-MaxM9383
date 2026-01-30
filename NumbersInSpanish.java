5c41696023773e85f708beaecab5e0e57bb4076e
MaxM9383 <mnm220@exeter.ac.uk>
2026-01-30 12:09:51 +0000

    NumbersInSpanish completed with if - else if - else

---
+++ NumbersInSpanish.java
@@ -0,0 +1,28 @@
+public class NumbersInSpanish {
+    public static void main(String[] args) {
+        // read the first argument passed to the program
+        int number = Integer.parseInt(args[0]); 
+        if (number == 1){
+            System.out.println("uno");
+        }
+        else if (number == 2){
+            System.out.println("duo");
+        }
+        else if (number == 2){
+            System.out.println("duo");
+        }
+        else if (number == 3){
+            System.out.println("tres");
+        }
+        else if (number == 4){
+            System.out.println("cuatro");
+        }
+        else if (number == 5){
+            System.out.println("cinco");
+        }
+        else{
+            System.out.println("idk");
+        }
+        
+    }
+ }
