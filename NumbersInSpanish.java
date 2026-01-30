d828fc0ca3c420de1ff91319297ec32bf312df60
MaxM9383 <mnm220@exeter.ac.uk>
2026-01-30 12:14:01 +0000

    NumbersInSpanish completed with switch-case (i got a little lazy)

--- NumbersInSpanish.java
+++ NumbersInSpanish.java
@@ -2,27 +2,13 @@ public class NumbersInSpanish {
     public static void main(String[] args) {
         // read the first argument passed to the program
         int number = Integer.parseInt(args[0]); 
-        if (number == 1){
-            System.out.println("uno");
+        switch(number){
+            case 1: System.out.println("uno"); break;
+            case 2: System.out.println("duo"); break;
+            case 3: System.out.println("tres"); break;
+            case 4: System.out.println("4"); break;
+            case 5: System.out.println("5"); break;
+            default: System.out.println("idk");
         }
-        else if (number == 2){
-            System.out.println("duo");
-        }
-        else if (number == 2){
-            System.out.println("duo");
-        }
-        else if (number == 3){
-            System.out.println("tres");
-        }
-        else if (number == 4){
-            System.out.println("cuatro");
-        }
-        else if (number == 5){
-            System.out.println("cinco");
-        }
-        else{
-            System.out.println("idk");
-        }
-        
     }
  }
